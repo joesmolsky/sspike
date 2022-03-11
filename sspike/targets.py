36644adf9194e43c230bf13db949fd4e92d02f31
joesmolsky <joesmolsky@gmail.com>
2022-03-11 10:08:53 -0500

    add self.snow_channels

--- sspike/targets.py
+++ sspike/targets.py
@@ -17,3 +17,15 @@ class Target:
             self.N_p = 4 * np.pi * _rho_p * _R_f**3 / 3
             # Number of electrons in radius R_f.
             self.N_e = self.N_p * 4.047
+            self.snow_channels = ['ibd',
+                                  'nue_e', 'nuebar_e', 'numu_e',
+                                  'numubar_e', 'nutau_e', 'nutaubar_e',
+                                  'nue_C12', 'nuebar_C12', 'nc_nue_C12',
+                                  'nc_nuebar_C12', 'nc_numu_C12',
+                                  'nc_numubar_C12', 'nc_nutau_C12',
+                                  'nc_nutaubar_C12',
+                                  'nue_C13',
+                                  'nc_nue_C13', 'nc_numu_C13', 'nc_nutau_C13',
+                                  'nc_nuebar_C13', 'nc_numubar_C13',
+                                  'nc_nutaubar_C13'
+                                  ]
