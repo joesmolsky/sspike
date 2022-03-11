45a4ee3c8a0d224304bc42cec7be289cde428826
joesmolsky <joesmolsky@gmail.com>
2022-03-11 10:47:26 -0500

    add docstring and sppike channels (basic, nc)

--- sspike/targets.py
+++ sspike/targets.py
@@ -6,7 +6,23 @@ log = getLogger(__name__)
 
 
 class Target:
-    """KamLAND specific information."""
+    """Target/detector specific information.
+
+    Parameters
+    ----------
+    name: str
+        Detector name in SNOwGLoBES (and sspike (copying SNOwGLoBES ( ;) ))).
+    N_p : float
+        Number of target protons.
+    N_e : float
+        Number of target electrons.
+    snow_channels : list of  str
+        Channels used in SNOwGLoBES simulations.
+    basic_channels : list of str
+        Channels for cross-checking sspike with SNOwGLoBES.
+    nc_channels : list of str
+        Neutrino-proton neutral-current interaction channels by flavor.
+    """
     def __init__(self, target):
         self.name = target
         if target == 'kamland':
@@ -17,7 +33,9 @@ class Target:
             self.N_p = 4 * np.pi * _rho_p * _R_f**3 / 3
             # Number of electrons in radius R_f.
             self.N_e = self.N_p * 4.047
-            self.snow_channels = ['ibd',
+            # SNOwGLoBES channels.
+            self.snow_channels = [
+                                  'ibd',
                                   'nue_e', 'nuebar_e', 'numu_e',
                                   'numubar_e', 'nutau_e', 'nutaubar_e',
                                   'nue_C12', 'nuebar_C12', 'nc_nue_C12',
@@ -29,3 +47,7 @@ class Target:
                                   'nc_nuebar_C13', 'nc_numubar_C13',
                                   'nc_nutaubar_C13'
                                   ]
+            # sspike channels.
+            self.nc_channels = ['nc_nue_p', 'nc_nuebar_p',
+                                'nc_nux_p', 'nc_nuxbar_p']
+            self.basic_channels = ['ibd', 'nue_e', 'nuebar_e', 'nux_e']
