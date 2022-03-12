689bdf956aa86d036cb273b8a66e2895aeddb6c5
joesmolsky <joesmolsky@gmail.com>
2022-03-12 13:33:32 -0500

    unit tests for pnut functions

---
+++ test/test_pnut.py
@@ -0,0 +1,33 @@
+from sspike.snowball import Snowball
+from sspike.targets import Target
+from sspike import pnut
+
+model = 'Nakazato_2013'
+progenitor = {'mass': 20,
+              'metal': 0.02,
+              't_rev': 300}
+transformation = 'NoTransformation'
+distance = 5
+
+sb = Snowball(model, progenitor, transformation, distance)
+target = Target('kamland')
+
+
+def test_snowglobes_events():
+    snowflakes = pnut.snowglobes_events(sb, target)
+    assert snowflakes[0] == '/Users/joe/src/gitjoe/sspike/snowballs'\
+                            '/N13-20-20-300/5kpc-NoTransformation'\
+                            '/snow-unsmeared.csv'
+    assert snowflakes[1] == '/Users/joe/src/gitjoe/sspike/snowballs'\
+                            '/N13-20-20-300/5kpc-NoTransformation'\
+                            '/snow-smeared.csv'
+
+
+def test_sspike_events():
+    sspikes = pnut.sspike_events(sb, target)
+    assert sspikes[0] == '/Users/joe/src/gitjoe/sspike/snowballs'\
+                         '/N13-20-20-300/5kpc-NoTransformation/'\
+                         'sspike-basic.csv'
+    assert sspikes[1] == '/Users/joe/src/gitjoe/sspike/snowballs'\
+                         '/N13-20-20-300/5kpc-NoTransformation/'\
+                         'sspike-nc.csv'
