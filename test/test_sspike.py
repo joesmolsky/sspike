467cf9cfd4bbdc5461d050cef9bc1d4cf092de52
joesmolsky <joesmolsky@gmail.com>
2022-03-20 09:03:40 -0500

    update expexted output

--- test/test_sspike.py
+++ test/test_sspike.py
@@ -2,7 +2,13 @@ import subprocess
 
 
 def test_sspike():
+    # Run Nakazato model from command line.
     sspike_com = ['sspike', 'Nakazato_2013', '-D', '10',
                   '-M', '20', '-Z', '0.02', '-R', '200']
-    assert subprocess.check_output(sspike_com) == b"Starting simulation.\n"\
-                                                  b"Job's done.\n"
+
+    # Expected print statements.
+    output = b"Starting simulation: Nakazato_2013 \t "\
+             b"{'mass': '20', 'metal': 0.02, 't_rev': '200'}.\n"\
+             b"Job's done.\n"
+
+    assert subprocess.check_output(sspike_com) == output
