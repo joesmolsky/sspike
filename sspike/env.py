ec79c36fe75e3d96345395d6399dbc67094fdbcf
joesmolsky <joesmolsky@gmail.com>
2022-04-20 11:54:22 -0400

    remove trailing '/' on dir names

--- sspike/env.py
+++ sspike/env.py
@@ -1,7 +1,7 @@
 # Install locations.
 # Location of snewpy models directory.
-models_dir = '/Users/joe/src/snewpy/models/'
-# SNOwGLoBES location.
-snowglobes_dir = "/Users/joe/src/gitjoe/snowglobes/"
+models_dir = '/Users/joe/src/snewpy/models'
+# SNOwGLoBES install location.
+snowglobes_dir = "/Users/joe/src/gitjoe/snowglobes"
 # sspike output directory.
-snowball_dir = "/Users/joe/src/gitjoe/sspike/out/"
+sspike_dir = "/Users/joe/src/gitjoe/sspike/out"
