96a7acccb3ca7a2255f47a6729d5ebddad2d287c
joesmolsky <joesmolsky@gmail.com>
2022-05-01 08:26:53 -0400

    docs change

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,13 +17,13 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'sspike'
-copyright = '2022, Joe Smolsky'
-author = 'Joe Smolsky'
+project = "sspike"
+copyright = "2022, Joe Smolsky"
+author = "Joe Smolsky"
 
 # The full version, including alpha/beta/rc tags
-with open('/Users/joe/src/gitjoe/sspike/sspike/_version.py', 'r') as f:
-    version = f.readline().split('=')[1].strip()
+with open("/Users/joe/src/gitjoe/sspike/sspike/_version.py", "r") as f:
+    version = f.readline().split("=")[1].strip()
 release = version
 
 
@@ -32,18 +32,15 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary", "sphinx.ext.napoleon"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.vscode']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".vscode"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,12 +48,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.vscode']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_favicon = '_static/spike.ico'
-html_logo = '_static/spike.png'
-html_static_path = ['_static']
+html_favicon = "_static/spike.ico"
+html_logo = "_static/spike.png"
+html_static_path = ["_static"]
